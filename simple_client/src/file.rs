c49b8b732f483c094b961e8dfa41ebd81e19f2e8
Parth Patel <parth@webcluesinfotech.com>
2025-05-27 11:30:09 +0530

    Added gitignore

---
+++ simple_client/src/file.rs
@@ -0,0 +1,44 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+const DIR_NAME: &str = "simple";
+const FILE_NAME: &str = "btc_data.json";
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PriceData {
+    prices: Vec<f64>,
+    average: f64,
+}
+
+impl PriceData {
+    pub fn new(prices: Vec<f64>, average: f64) -> Self {
+        Self {
+            prices,
+            average,
+        }
+    }
+}
+
+pub fn write_data_to_file(price_data: &PriceData) {
+    let path_exists = fs::exists(DIR_NAME).unwrap();
+    if !path_exists {
+        fs::create_dir(DIR_NAME).expect("Error in creating directory");
+    }
+    let file_path = format!("{}/{}", DIR_NAME, FILE_NAME);
+    let json_string = serde_json::to_string(price_data).expect("Error in serializing");
+    fs::write(file_path, json_string).expect("Error in writing file");
+}
+pub fn get_data_from_file() {
+    let file_path = format!("{}/{}", DIR_NAME, FILE_NAME);
+    let path_exists = fs::exists(&file_path).unwrap();
+    if path_exists {
+        if let Ok(json_string) = fs::read_to_string(file_path) {
+            let price_data: PriceData = serde_json::from_str(json_string.as_str()).expect("Error in deserializing");
+            println!("Read complete: {:#?}", price_data);
+        }else {
+            println!("No data found, please run the cache mode first.");
+        }   
+    } else {
+        println!("No data found, please run the cache mode first.");
+    }
+}
--- simple_client/src/main.rs
+++ simple_client/src/main.rs
@@ -1,3 +1,98 @@
-fn main() {
-    println!("Hello, world!");
+mod file;
+
+use clap::{Arg, Command};
+use futures_util::StreamExt;
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
+
+const WEB_SOCKET_URL_BTC: &str = "wss://stream.binance.com:443/ws/btcusdt@miniTicker";
+const CONNECTION_TIMEOUT: u64 = 5;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let matches = Command::new("simple")
+        .about("simple client that can run the two following commands: 1) ./ =10 2) ./simple --mode=read")
+        .arg(Arg::new("mode")
+            .short('m')
+            .long("mode")
+            .required(true)
+            .help("Mode should be 'cache' or 'read'")
+        )
+        .arg(Arg::new("times")
+            .short('t')
+            .long("times")
+            .required(false)
+            .help("Number of times to fetch the price")
+            .default_value("10")
+        ).get_matches();
+
+    let mode = matches.get_one::<String>("mode").unwrap();
+    match mode.as_str() {
+       "cache" => {
+           let times = matches.get_one::<String>("times").unwrap();
+           match times.parse::<usize>() {
+               Ok(times) => {
+                   get_btc_price(times).await;
+               },
+               Err(e) => {
+                   println!("Error in times argument: {}",e);
+               }
+           }
+       },
+       "read" => {
+           file::get_data_from_file();
+       },
+       _ => {
+           println!("Error: mode should be either 'cache' or 'read'");
+       }
+   }
+}
+
+async fn get_btc_price(times: usize) {
+    let connection = tokio_tungstenite::connect_async(WEB_SOCKET_URL_BTC);
+    let timeout_duration = tokio::time::Duration::from_secs(CONNECTION_TIMEOUT);
+    let result = tokio::time::timeout(timeout_duration, connection).await;
+    match result {
+        Ok(connection) => {
+            if connection.is_ok() {
+                let (stream, _response) = connection.ok().unwrap();
+                let (_write, read) = stream.split();
+                let prices = Arc::new(Mutex::new(Vec::new()));
+                read.take(times).for_each(|msg| {
+                    let prices = prices.clone();
+                    async move {
+                        if msg.is_ok() {
+                            let message = msg.ok().unwrap().to_string();
+                            let btc_ticker = serde_json::from_str::<BtcTicker>(message.as_str());
+                            if btc_ticker.is_ok() {
+                                let price = btc_ticker.ok().unwrap().c.parse::<f64>().unwrap();
+                                prices.lock().unwrap().push(price);
+                                println!("Received btc Price: {}", price);
+                            } else {
+                                println!("Error in deserializing: {:?}", btc_ticker.err().unwrap());
+                            }
+                        } else {
+                            println!("Error in message: {:?}", msg.err().unwrap());
+                        }
+                    }
+                }).await;
+                let data: Vec<f64> = prices.lock().unwrap().iter().map(|x| *x).collect();
+                let average = data.iter().sum::<f64>() / data.len() as f64;
+                let price_data = file::PriceData::new(data, average);
+                file::write_data_to_file(&price_data);
+                println!("Cache complete. The average USD price of BTC is: {}", average);
+            } else {
+                println!("Error connecting to the websocket {}", connection.err().unwrap());
+            }
+        },
+        Err(_) => {
+            println!("WebSocket connection timed out.");
+        }       
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct BtcTicker {
+    s: String,
+    c: String,
 }
